147d671949fdc0c13a5d102af348a063d45a5adb
ArturNedava <121451856+ArturNedava@users.noreply.github.com>
2024-01-14 17:42:25 +0100

    First commit

---
+++ lando.js
@@ -0,0 +1,44 @@
+// const users = [
+//     {name: "Mango", eyeColor: "brown", gender: "maskulin", isAktive: true, email: "Mango@.com", age: 12},
+//     {name: "Poly", eyeColor: "blue", gender: "feminim", isAktive: false, email: "Poly@.com", age: 11},
+//     {name: "Lily", eyeColor: "blue", gender: "maskulin", isAktive: true, email: "Lily@.com", age: 10},
+//     {name: "Ajax", eyeColor: "brown", gender: "feminim", isAktive: false, email: "Ajax@.com", age: 9}
+// ]
+
+// const names = users.map(user => user.name)
+// console.log(names)
+
+
+//2
+// const brownEyes = users.filter(user => user.eyeColor === "brown")
+// const blueEyes = users.filter(user => user.eyeColor === "blue")
+
+// console.log(brownEyes)
+// console.log(blueEyes)
+
+
+//3
+// const maskulin = users.filter(user => user.gender === "maskulin")
+// const feminim = users.filter(user => user.gender === "feminim")
+
+// console.log(maskulin)
+// console.log(feminim)
+
+
+//4
+// const isAktiveUsers = users.filter(user => !user.isAktive )
+// console.log(isAktiveUsers)
+
+
+//5
+// const FindUser = users.find(user => user.email === "Mango@.com")
+// console.log(FindUser)
+
+//6
+
+// const ageUser = users.filter(user => user.age > 8 < 13)
+// console.log(ageUser)
+
+
+
+
